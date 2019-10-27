ccf19f521e918641cd8cde555a72dbcec2815d86
Baker Satou <bsatou@oa-g3.21-school.ru>
2019-10-27 17:30:25 +0300

    add some stuff

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/akorshun/roger_site roger_site
-cp /home/roger/roger_site/index.html /var/www/putinvor.com/html
+cp /home/roger/roger_site/index.html /var/www/html
 rm -rf /home/roger/roger_site
